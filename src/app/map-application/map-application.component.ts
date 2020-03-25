6d1e2497c2c59f138d4688cf63d142e9cf774217
cKoert <christian.koert@outlook.com>
2020-03-25 11:17:47 +0100

    Adding Leaflet Map to the map Component
    Includes layout improvements

--- src/app/map-application/map-application.component.ts
+++ src/app/map-application/map-application.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
+
+
 
 @Component({
   selector: 'app-map-application',
   templateUrl: './map-application.component.html',
   styleUrls: ['./map-application.component.scss']
 })
-export class MapApplicationComponent implements OnInit {
+
+export class MapApplicationComponent implements AfterViewInit  {
+  private map;
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit(): void {
+    this.initMap();
   }
 
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 51.4465,7.2721],
+      zoom: 15
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
 }
