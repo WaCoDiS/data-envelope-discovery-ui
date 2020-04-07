4c5defcd74e18905981be274dc2dbe6fb22494c0
cKoert <christian.koert@outlook.com>
2020-04-07 09:44:21 +0200

    Adding method to react on events

--- src/app/map-application/map-application.component.ts
+++ src/app/map-application/map-application.component.ts
@@ -1,7 +1,7 @@
 import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 import { Draw } from 'leaflet';
-
+import 'leaflet-draw';
 
 @Component({
   selector: 'app-map-application',
@@ -11,12 +11,22 @@ import { Draw } from 'leaflet';
 
 export class MapApplicationComponent implements AfterViewInit  {
   private map;
+  private drawnItems;
+  private drawControl;
 
 
   drawOptions = {
-    position: 'topright',
+    draw: {
+//      polygon: false,
+//      polyline: false,
+//      circle: false,
+//      marker: false,
+//      circlemarker: false
+    },
+    edit: {
+      featureGroup: this.drawnItems
+    }
   };
-
 
   constructor() { }
 
@@ -30,6 +40,7 @@ export class MapApplicationComponent implements AfterViewInit  {
       center: [ 51.4465,7.2721],
       zoom: 15,
     });
+
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
@@ -41,7 +52,15 @@ export class MapApplicationComponent implements AfterViewInit  {
     this.createLayerSelection(tiles);
 
 
+    var polygon = L.polygon([
+      [51.509, -0.08],
+      [51.503, -0.06],
+      [51.51, -0.047]
+    ]).addTo(this.map);
+
+
   }
+
 
   private createLayerSelection(tiles: L.TileLayer) {
     var baseMaps = {
@@ -51,27 +70,41 @@ export class MapApplicationComponent implements AfterViewInit  {
     L.control.layers(baseMaps).addTo(this.map);
   }
 
-  private createDrawToolbar() {
-    // FeatureGroup is to store editable layers
-    var drawnItems = new L.FeatureGroup();
-    this.map.addLayer(drawnItems);
 
-    var drawControl = new L.Control.Draw({
-      draw: {
-//      polygon: false,
-//      polyline: false,
-//      circle: false,
-//      marker: false,
-//      circlemarker: false
-      },
-      edit: {
-        featureGroup: drawnItems
-      }
+
+  private createDrawToolbar() {
+
+    // FeatureGroup is to store editable layers
+    this.drawnItems = new L.FeatureGroup();
+    this.map.addLayer(this.drawnItems);
+
+    // Initialise the draw control and pass it the FeatureGroup of editable layers
+    this.drawControl = new L.Control.Draw(this.drawOptions);
+    this.map.addControl(this.drawControl);
+
+
+
+    this.map.on(L.Draw.Event.CREATED, function (e) {
+      //var layers = e.layers;
+      var type = e.layerType;
+      alert(type);
+      var layer = e.layer;
+      alert(layer);
+      //this.map.addLayer(layer);
+      //this.map.addLayer(this.drawnItems);
+      this.drawnItems.addLayer(layer)
+      layer.addTo(this.drawnItems);
+      layer.addTo(this.map);
+  });
+
+
+    this.map.on('click', function(ev) {
+      //alert(ev.latlng); // ev is an event object (MouseEvent in this case)
     });
-    this.map.addControl(drawControl);
+
   }
 
-  
+
   onDrawReady(drawControl: L.Control.Draw) {
     alert("hallo")
   }
