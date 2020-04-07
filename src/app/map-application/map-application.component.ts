6d08d6bd07ceb3ae4cccdf17b2a8c80c94e76156
cKoert <christian.koert@outlook.com>
2020-04-07 10:55:48 +0200

    Correct map, so that only ngx-leaflet is used

--- src/app/map-application/map-application.component.ts
+++ src/app/map-application/map-application.component.ts
@@ -1,7 +1,5 @@
 import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
-import { Draw } from 'leaflet';
-import 'leaflet-draw';
 
 @Component({
   selector: 'app-map-application',
@@ -9,103 +7,38 @@ import 'leaflet-draw';
   styleUrls: ['./map-application.component.scss']
 })
 
-export class MapApplicationComponent implements AfterViewInit  {
-  private map;
-  private drawnItems;
-  private drawControl;
+export class MapApplicationComponent  {
 
+  options = {
+    layers: [
+      L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: 'Open Street Map' })
+    ],
+    zoom: 5,
+    center: L.latLng({ lat: 46.879966, lng: -121.726909 })
+  };
 
   drawOptions = {
+    position: 'topright',
     draw: {
-//      polygon: false,
-//      polyline: false,
-//      circle: false,
-//      marker: false,
-//      circlemarker: false
-    },
-    edit: {
-      featureGroup: this.drawnItems
+      marker: {
+        icon: L.icon({
+          iconSize: [ 25, 41 ],
+          iconAnchor: [ 13, 41 ],
+          iconUrl: '2273e3d8ad9264b7daa5bdbf8e6b47f8.png',
+          shadowUrl: '44a526eed258222515aa21eaffd14a96.png'
+        })
+      }
     }
   };
 
-  constructor() { }
-
-  ngAfterViewInit(): void {
-    this.initMap();
+  public onDrawCreated(e: any) {
+    // tslint:disable-next-line:no-console
+    console.log('Draw Created Event!');
   }
 
-
-  private initMap():void{
-    this.map = L.map('map', {
-      center: [ 51.4465,7.2721],
-      zoom: 15,
-    });
-
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
-    tiles.addTo(this.map);
-
-
-    this.createDrawToolbar();
-    this.createLayerSelection(tiles);
-
-
-    var polygon = L.polygon([
-      [51.509, -0.08],
-      [51.503, -0.06],
-      [51.51, -0.047]
-    ]).addTo(this.map);
-
-
+  public onDrawStart(e: any) {
+    // tslint:disable-next-line:no-console
+    console.log('Draw Started Event!');
   }
 
-
-  private createLayerSelection(tiles: L.TileLayer) {
-    var baseMaps = {
-      "OSM": tiles
-    };
-
-    L.control.layers(baseMaps).addTo(this.map);
-  }
-
-
-
-  private createDrawToolbar() {
-
-    // FeatureGroup is to store editable layers
-    this.drawnItems = new L.FeatureGroup();
-    this.map.addLayer(this.drawnItems);
-
-    // Initialise the draw control and pass it the FeatureGroup of editable layers
-    this.drawControl = new L.Control.Draw(this.drawOptions);
-    this.map.addControl(this.drawControl);
-
-
-
-    this.map.on(L.Draw.Event.CREATED, function (e) {
-      //var layers = e.layers;
-      var type = e.layerType;
-      alert(type);
-      var layer = e.layer;
-      alert(layer);
-      //this.map.addLayer(layer);
-      //this.map.addLayer(this.drawnItems);
-      this.drawnItems.addLayer(layer)
-      layer.addTo(this.drawnItems);
-      layer.addTo(this.map);
-  });
-
-
-    this.map.on('click', function(ev) {
-      //alert(ev.latlng); // ev is an event object (MouseEvent in this case)
-    });
-
-  }
-
-
-  onDrawReady(drawControl: L.Control.Draw) {
-    alert("hallo")
-  }
 }
