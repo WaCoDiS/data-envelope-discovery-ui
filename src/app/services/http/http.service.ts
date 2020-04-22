f66846dd953279117e43a7dc2ba0e36eeff1e031
Sebastian Drost <s.drost@52north.org>
2020-04-22 12:25:22 +0200

    Remove authorization from HTTP headers

--- src/app/services/http/http.service.ts
+++ src/app/services/http/http.service.ts
@@ -8,7 +8,6 @@ import { HttpErrorHandler, HandleError } from '../http-error-handler.service';
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type':  'application/json',
-    'Authorization': 'my-auth-token'
   })
 };
 
