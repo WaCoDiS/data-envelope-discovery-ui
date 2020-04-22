cdeb8c09c57f73fc0b76ee82d1d718ddafd17549
Tim Kurowski <tim.kurowski@hs-bochum.de>
2020-04-22 11:21:58 +0200

    put error handler into http-service

--- src/app/services/http/http.service.ts
+++ src/app/services/http/http.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Subject, Observable } from 'rxjs';
+import { Subject, Observable, throwError } from 'rxjs';
 import * as sourceType from '../../source-type-interfaces';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
-import { HttpErrorHandler, HandleError } from '../http-error-handler.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -18,17 +18,32 @@ const httpOptions = {
 export class HttpService {
 
   dataAccessApiUrl = 'http://localhost:8080/dataAccess/dataenvelopes/search';  // URL to data access api search
-  private handleError: HandleError;
 
   constructor(
     private http: HttpClient) {
   }
 
   searchDataEnvelope (dataEnvelope: sourceType.DataEnvelope): Observable<sourceType.DataEnvelope> {
-    return this.http.post<sourceType.Copernicus>(this.dataAccessApiUrl, dataEnvelope, httpOptions)
+    return this.http.post<sourceType.DataEnvelope>(this.dataAccessApiUrl, dataEnvelope, httpOptions)
       .pipe(
-        catchError(this.handleError('searchDataEnvelope', dataEnvelope))
+        catchError(this.handleError)
       );
   }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
 
 }
