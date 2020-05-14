1a8426ba82ff4ff522bad22927a21eed22018903
cKoert <christian.koert@outlook.com>
2020-05-14 08:21:19 +0200

    Moving result button to source-type-component

--- src/app/source-type/result/result.component.ts
+++ src/app/source-type/result/result.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit, PipeTransform } from '@angular/core';
-import * as sourceType from '../../source-type-interfaces';
-import { HttpService } from 'src/app/services/http/http.service';
-import { ParameterService } from 'src/app/services/parameter-service/parameter-service.service';
-import { FormControl } from '@angular/forms';
 
 
 @Component({
@@ -12,28 +8,12 @@ import { FormControl } from '@angular/forms';
 })
 
 export class ResultComponent implements OnInit {
-  dataEnvelopes: sourceType.DataEnvelopeResult[];
-  filter = new FormControl('');
 
-  resultPressed = false;
-
-  constructor(private httpService: HttpService, private parameterService: ParameterService) {
-
-  }
 
   ngOnInit() {
   }
 
 
-  sendRequest() {
-    this.resultPressed = true;
-    this.dataEnvelopes = new Array();
-    console.log(this.parameterService.getDataEnvelope())
-    var ergebnis = this.httpService.searchDataEnvelope(this.parameterService.getDataEnvelope());
-    ergebnis.subscribe(val => console.log(val));
-    ergebnis.subscribe(dataEnvelope => this.dataEnvelopes.push(dataEnvelope));
-    console.log(this.dataEnvelopes);
-  }
 
 }
 
--- src/app/source-type/source-type.component.ts
+++ src/app/source-type/source-type.component.ts
@@ -4,6 +4,9 @@ import { Copernicus } from '../source-type-interfaces';
 import { Dwd } from '../source-type-interfaces';
 import { WacodisProduct } from '../source-type-interfaces';
 import { ParameterService } from '../services/parameter-service/parameter-service.service';
+import * as sourceType from '../source-type-interfaces';
+import { HttpService } from 'src/app/services/http/http.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-source-type',
@@ -12,19 +15,24 @@ import { ParameterService } from '../services/parameter-service/parameter-servic
 })
 export class SourceTypeComponent{
   componentTitel = "Source type"
-
   sourceTypeSelection = 'SensorWeb';
-  //showcontent:boolean=false;
+  dataEnvelopes: sourceType.DataEnvelopeResult[];
+  resultPressed = false;
 
-
-  /*showContent(){
-    this.showcontent=this.radioModel;
-  }
-*/
-
-constructor(public parameterService: ParameterService) { }
+constructor(public parameterService: ParameterService, private httpService: HttpService) { }
   sendChoosenSourceType(event: any){
    this.parameterService.changeSourceType(event);
+  }
+
+
+  sendRequest() {
+    this.resultPressed = true;
+    this.dataEnvelopes = new Array();
+    console.log(this.parameterService.getDataEnvelope())
+    var ergebnis = this.httpService.searchDataEnvelope(this.parameterService.getDataEnvelope());
+    ergebnis.subscribe(val => console.log(val));
+    ergebnis.subscribe(dataEnvelope => this.dataEnvelopes.push(dataEnvelope));
+    console.log(this.dataEnvelopes);
   }
 
 }
