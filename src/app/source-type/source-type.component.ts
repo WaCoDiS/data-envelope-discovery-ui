a9d49bfae97df22a827840ee8444fba7dce67f3a
Tim Kurowski <tim.kurowski@hs-bochum.de>
2020-05-20 10:21:18 +0200

    remove result service

--- src/app/source-type/source-type.component.ts
+++ src/app/source-type/source-type.component.ts
@@ -2,8 +2,6 @@ import { Component, EventEmitter, Output} from '@angular/core';
 import { ParameterService } from '../services/parameter-service/parameter-service.service';
 import * as sourceType from '../source-type-interfaces';
 import { HttpService } from 'src/app/services/http/http.service';
-import {ResultService} from 'src/app/services/result/result.service';
-import {MapApplicationComponent} from '../map-application/map-application.component';
 
 @Component({
   selector: 'app-source-type',
@@ -14,10 +12,11 @@ export class SourceTypeComponent{
   componentTitel = "Source type"
   sourceTypeSelection = 'SensorWeb';
   resultPressed = false;
+  dataEnvelopes: sourceType.DataEnvelopeResult[];
 
   @Output() results = new EventEmitter<sourceType.DataEnvelopeResult[]>();
 
-constructor(public parameterService: ParameterService, private httpService: HttpService, private resultService: ResultService) { }
+constructor(public parameterService: ParameterService, private httpService: HttpService) { }
 
   sendChoosenSourceType(event: any){
    this.parameterService.changeSourceType(event);
@@ -25,12 +24,14 @@ constructor(public parameterService: ParameterService, private httpService: Http
 
 
   sendRequest() {
+    this.dataEnvelopes =  new Array();
     this.resultPressed = true;
     console.log(this.parameterService.getDataEnvelope())
     var ergebnis = this.httpService.searchDataEnvelope(this.parameterService.getDataEnvelope());
     ergebnis.subscribe(val => console.log(val));
-    ergebnis.subscribe(dataEnvelope => this.resultService.addResult(dataEnvelope));
-    this.results.emit(this.resultService.getResults());
+    ergebnis.subscribe(dataEnvelope => this.dataEnvelopes.push(dataEnvelope));
+    console.log(this.dataEnvelopes);
+    this.results.emit(this.dataEnvelopes);
   }
 
 }
